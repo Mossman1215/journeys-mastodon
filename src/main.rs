b4f00015a5e73bb7ab5b263f88e6a6114f281fcb
Moss Cantwell <mossman2142@gmail.com>
2024-06-09 00:35:27 +1200

    first working version

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::{collections::HashMap, env, fs::File, io::{BufReader, Read}, process, str::FromStr, sync::Arc};
-
+use std::{collections::HashMap, env, fs::File, io::{BufReader, Read}, ops::Sub, process, str::FromStr, sync::Arc};
+use std::time::{Duration, SystemTime};
+use chrono::{DateTime, Local, NaiveTime, TimeDelta};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
@@ -24,27 +25,59 @@ async fn main() {
         println!("failed to find ACCESS_TOKEN. Exiting...");
         process::exit(1);
     }
+    let dt1: DateTime<Local> = Local::now();
+    let now = TimeDelta::new(dt1.timestamp(),0).unwrap();
+    let resp = reqwest::get("https://www.journeys.nzta.govt.nz/assets/map-data-cache/delays.json")
+        .await.unwrap().text().await.expect("failed to fetch nzta data");
+    let delays: Value = serde_json::from_str(&resp).expect("parse failed");
+    //Duration::new(900, 0);
+    //Duration::new(lastUpdated)
+    let lastupdate = TimeDelta::new(delays["lastUpdated"].as_i64().unwrap(),0).unwrap();
+    let duration = TimeDelta::new(300, 0).unwrap();
+    //if now sub lastupdated > 300 check for closures with matching properties
+    if  now-lastupdate>duration{
+        for feature in delays["features"].as_array().unwrap() {
+            let desc = feature["properties"].as_object().unwrap()["EventDescription"].as_str().unwrap();
+            if desc == "Crash"{
+            let message = format!("{}\n{}\n{}\n{}",
+                feature["properties"].as_object().unwrap()["Name"].as_str().unwrap(),
+                feature["properties"].as_object().unwrap()["LocationArea"].as_str().unwrap(),
+                feature["properties"].as_object().unwrap()["EventComments"].as_str().unwrap(),
+                feature["properties"].as_object().unwrap()["LastUpdatedNice"].as_str().unwrap()
+            );
+            let mut map = HashMap::new();
+            println!("sending message to mastodon");
+            map.insert("status", message.as_str());
+            map.insert("visibility", "public");
 
-    let resp = reqwest::get("https://httpbin.org/ip")
-        .await.unwrap()
-        .json::<HashMap<String, String>>()
-        .await;
-    println!("{resp:#?}");
+            let client = reqwest::Client::new();
+            let res2 = client.post("https://g2s.mountainmoss.nz/api/v1/statuses")
+                .bearer_auth(auth_token.access_token.clone())
+                .header("User-Agent", "journeys-mastodon")
+                .json(&map)
+                .send()
+                .await.unwrap();
+            if res2.status() != 200 {
+                println!("error happened: {},{}",res2.status(),res2.text().await.unwrap())
+              }
+            }
+        }
+    }
 }
 
 #[test]
 fn property_parse() {
-        // Open the file in read-only mode with buffer.
-        let mut file = File::open("fixtures/delays.json").expect("should pass");
-    
-        let mut data = String::new();
-        file.read_to_string(&mut data).unwrap();
-        let poo: Value = serde_json::from_str(&data).expect("parse failed");
-        assert!(poo.is_object());
-        assert!(poo["features"].is_array());
-        assert_eq!(poo["features"].as_array().unwrap().len(),143);
-        let blerp = poo["features"].as_array().unwrap()[0].clone();
-        assert!((blerp["properties"].as_object().unwrap())["Name"].as_str() == Some("Road Closure: SH 1 Onewa Rd On-ramp Southbound"));
-        assert!(blerp["properties"].as_object().unwrap()["LastEdited"].as_str() == Some("2024-06-02 20:03:35"));
-        assert!(poo["lastUpdated"].as_i64() == Some(1717315415));
+    // Open the file in read-only mode with buffer.
+    let mut file = File::open("fixtures/delays.json").expect("should pass");
+
+    let mut data = String::new();
+    file.read_to_string(&mut data).unwrap();
+    let delays: Value = serde_json::from_str(&data).expect("parse failed");
+    assert!(delays.is_object());
+    assert!(delays["features"].is_array());
+    assert_eq!(delays["features"].as_array().unwrap().len(),143);
+    let test_event = delays["features"].as_array().unwrap()[0].clone();
+    assert!((test_event["properties"].as_object().unwrap())["Name"].as_str() == Some("Road Closure: SH 1 Onewa Rd On-ramp Southbound"));
+    assert!(test_event["properties"].as_object().unwrap()["LastEdited"].as_str() == Some("2024-06-02 20:03:35"));
+    assert!(delays["lastUpdated"].as_i64() == Some(1717315415));
 }
