52e159b14b50e4ac53a75bdd175fa41af3fe32fe
Moss Cantwell <m.cantwell@gns.cri.nz>
2024-10-13 21:38:20 +1300

    4 min threshold

--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,7 @@ async fn main() {
     //Duration::new(900, 0);
     //Duration::new(lastUpdated)
     let lastupdate = TimeDelta::new(delays["lastUpdated"].as_i64().unwrap(),0).unwrap();
-    let duration = TimeDelta::new(150, 0).unwrap();
+    let duration = TimeDelta::new(240, 0).unwrap();
     let re = Regex::new(r"SH\s[0-9]+").unwrap();
     //if now sub lastupdated > 300 check for closures with matching properties
     if  now-lastupdate<duration{
