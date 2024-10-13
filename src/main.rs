90b00a1be5922c92d6ef09fa3d15f9e7bb609f77
Moss Cantwell <mossman2142@gmail.com>
2024-10-13 18:07:42 +1300

    include second interval check because the first one was not strict enough

--- src/main.rs
+++ src/main.rs
@@ -43,7 +43,8 @@ async fn main() {
             let desc = feature["properties"].as_object().unwrap()["EventDescription"].as_str().unwrap();
             let last_edit_time = NaiveDateTime::parse_from_str( feature["properties"].as_object().unwrap()["LastEdited"].as_str().unwrap(),"%Y-%m-%d %H:%M:%S").unwrap();
             let last_edit_stamp = TimeDelta::new(last_edit_time.and_local_timezone(FixedOffset::east_opt(12*3600).unwrap()).unwrap().timestamp(),0).unwrap();
-            if desc == "Crash"&& now - last_edit_stamp<duration {
+            let last_update_stamp = TimeDelta::new(feature["properties"].as_object().unwrap()["lastUpdated"].as_i64().unwrap(), 0).unwrap();
+            if desc == "Crash" && now - last_edit_stamp<duration && now - last_update_stamp<duration{
                 let m = re.find(feature["properties"].as_object().unwrap()["LocationArea"].as_str().unwrap());
                 let mut highway_hash = String::from_str("").unwrap();
                 match m {
@@ -122,4 +123,21 @@ fn parse_regions() {
     let regions = FeatureCollection::try_from(geojson).unwrap();
     assert_eq!(regions.features.len(),17);
     assert_eq!(regions.features[8].property("REGC2023_1").unwrap(),"Wellington Region")
+}
+#[test]
+fn property_parse2() {
+    // Open the file in read-only mode with buffer.
+    let mut file = File::open("fixtures/delays2.json").expect("should pass");
+
+    let mut data = String::new();
+    file.read_to_string(&mut data).unwrap();
+    let delays: Value = serde_json::from_str(&data).expect("parse failed");
+    assert!(delays.is_object());
+    assert!(delays["features"].is_array());
+    assert_eq!(delays["features"].as_array().unwrap().len(),166);
+    let test_event = delays["features"].as_array().unwrap()[0].clone();
+    assert!((test_event["properties"].as_object().unwrap())["Name"].as_str() == Some("Road Closure: SH 1 Oteha Valley to Silverdale, Northbound"));
+    assert!(test_event["properties"].as_object().unwrap()["LastEdited"].as_str() == Some("2024-10-13 16:50:41"));
+    assert!(test_event["properties"].as_object().unwrap()["lastUpdated"].as_i64() == Some(1728791441));
+    assert!(delays["lastUpdated"].as_i64() == Some(1728793892));
 }
